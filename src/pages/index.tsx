88ac638981aa5040a1ed73fadc18ac4f1cf7dbc8
Emi2710 <deadourfreedom@gmail.com>
2023-03-01 13:06:42 +0100

    app setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        
+      </Head>
+      <main>
+        <h1 className='bg-red-500'>Hey this is a text</h1>
+      </main>
+    </>
+  )
+}
