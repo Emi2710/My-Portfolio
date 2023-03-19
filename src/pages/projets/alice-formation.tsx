cde4f22d7bf22431fae942ae03274a35519df742
Emi2710 <deadourfreedom@gmail.com>
2023-03-19 15:34:35 +0100

    fixing minor errors for deployment #2

--- src/pages/projets/alice-formation.tsx
+++ src/pages/projets/alice-formation.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Layout from '../../../layout/Layout'
 
+import Image from 'next/image'
+
+import alice1 from '../../../assets/alice1.png'
+import alice2 from '../../../assets/alice2.png'
+
 type Props = {}
 
 export default function aliceFormation({}: Props) {
@@ -8,10 +13,10 @@ export default function aliceFormation({}: Props) {
     <Layout>
         <div className='jakarta py-16 px-5 text-lg leading-9'>
             <p>Alice Formation est un site que j&apos;ai d&eacute;velopp&eacute; pour aider ma cliente &agrave; guider les personnes int&eacute;ress&eacute;es par les m&eacute;tiers du web et leur faire d&eacute;couvrir les diff&eacute;rentes opportunit&eacute;s qui existent dans ce domaine.</p>
-            <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/bd06df0d-fbbd-4d90-9fb7-0c8f7403c0a1/www.alice-formation.com__%284%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230319%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230319T110522Z&X-Amz-Expires=86400&X-Amz-Signature=95b078697233691a7afc7e586aff0e3460c970a5c5820ffca12f3c69edf42b45&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22www.alice-formation.com_%2520%284%29.png%22&x-id=GetObject" className='w-100 md:w-1/2 m-auto my-5' alt="écran de connexion sportelia" />
+            <Image src={alice1} className='w-100 md:w-1/2 m-auto my-5' alt="écran de connexion sportelia" />
            
             <p>En tant que d&eacute;veloppeuse web, j&apos;ai mis &agrave; profit mes comp&eacute;tences pour cr&eacute;er un site qui offre une exp&eacute;rience utilisateur fluide et intuitive. J&apos;y ai int&eacute;gr&eacute; un syst&egrave;me de gestion de contenu pour aider Alice &agrave; publier facilement ses articles, les g&eacute;rer et les modifier &agrave; tout moment. <span>Il est possible de parcourir les articles par cat&eacute;gories et par mots-cl&eacute;s pour trouver des informations sur les m&eacute;tiers qui int&eacute;ressent le visiteur du site.</span></p>
-            <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/7d8625db-eedd-4679-b828-b0cba675e905/www.alice-formation.com_formation_%281%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230319%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230319T110745Z&X-Amz-Expires=86400&X-Amz-Signature=ba640dcea8a908ad0869e419f6a85adf0b6d5c65b3ce4616a83d9f12d4dcb742&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22www.alice-formation.com_formation%2520%281%29.png%22&x-id=GetObject" className='w-100 md:w-1/2 m-auto my-5' alt="écran de connexion sportelia" />
+            <Image src={alice2} className='w-100 md:w-1/2 m-auto my-5' alt="écran de connexion sportelia" />
             
             <p>J&apos;ai &eacute;galement veill&eacute; &agrave; ce que le site soit facilement accessible sur tous les types d&apos;appareils, notamment les smartphones et les tablettes. Cela signifie que les visiteurs peuvent acc&eacute;der &agrave; notre contenu o&ugrave; qu&apos;ils se trouvent, &agrave; tout moment de la journ&eacute;e.</p>
             <p className='mt-8'>Je suis passionn&eacute; par le d&eacute;veloppement web et j&apos;ai &agrave; c&oelig;ur de cr&eacute;er des sites qui sont &agrave; la fois fonctionnels et esth&eacute;tiques. J&apos;ai donc travaill&eacute; dur pour concevoir un site qui est &agrave; la fois pratique et &eacute;l&eacute;gant. J&apos;ai utilis&eacute; les derni&egrave;res technologies pour m'assurer que le site est rapide et r&eacute;actif, ce qui permet une exp&eacute;rience utilisateur fluide.</p>
--- src/pages/projets/sportelia.tsx
+++ src/pages/projets/sportelia.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Layout from '../../../layout/Layout'
 
+import Image from 'next/image'
+
+import sportelia1 from '../../../assets/sportelia1.png'
+import sportelia2 from '../../../assets/sportelia2.png'
+
 type Props = {}
 
 export default function sportelia({}: Props) {
@@ -8,11 +13,11 @@ export default function sportelia({}: Props) {
     <Layout>
       <div className='jakarta py-16 px-5 text-lg leading-9'>
         
-        <p className='mb-8'>J&apos;ai d&eacute;velopp&eacute; une application web de gestion de franchises sportives pour les propri&eacute;taires de marques de sport qui cherchent &agrave; optimiser la gestion de leurs franchises. Cette application permet aux propri&eacute;taires de marques de sport d&apos;ajouter facilement de nouvelles franchises en quelques clics seulement, et de g&eacute;rer leurs droits d'acc&egrave;s &agrave; l&apos;application.</p>
-        <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/e7bfefbc-7633-4394-ac2e-c7e618c92d30/sportelia.herokuapp.com_login_%282%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230319%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230319T104454Z&X-Amz-Expires=86400&X-Amz-Signature=2e87245debec99fb7b93e74a8fc8fdc7880122eae2e6fef1c52d9b153f66c3d1&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22sportelia.herokuapp.com_login%2520%282%29.png%22&x-id=GetObject" className='w-100 md:w-1/2 m-auto' alt="écran de connexion sportelia" />
+        <p className='mb-8'>J&apos;ai d&eacute;velopp&eacute; une application web de gestion de franchises sportives pour les propri&eacute;taires de marques de sport qui cherchent &agrave; optimiser la gestion de leurs franchises. Cette application permet aux propri&eacute;taires de marques de sport d&apos;ajouter facilement de nouvelles franchises en quelques clics seulement, et de g&eacute;rer leurs droits d&apos;acc&egrave;s &agrave; l&apos;application.</p>
+        <Image src={sportelia1} className='w-100 md:w-1/2 m-auto' alt="écran de connexion sportelia" />
         
         <p className='mb-8'>La gestion des franchises est simplifi&eacute;e gr&acirc;ce &agrave; des tableaux de bord personnalisables. Vous pouvez suivre facilement l&apos;activit&eacute; de chaque franchise et analyser les performances de vos structures en temps r&eacute;el. Notre application permet &eacute;galement une gestion fine des droits d&apos;acc&egrave;s, vous pouvez d&eacute;finir les modules auxquels chaque utilisateur a acc&egrave;s et m&ecirc;me restreindre certains acc&egrave;s en fonction de son r&ocirc;le ou de sa position dans la franchise.</p>
-        <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d54d59a4-b6b3-4707-ada1-bb1ac7c6392e/Sportelia_-_Google_Chrome_29_10_2022_17_16_10.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230319%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230319T104732Z&X-Amz-Expires=86400&X-Amz-Signature=8be442764ef2e0a40e89d03b60c2e876d0b6d875bc8f2ec072c155283fce15e0&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Sportelia%2520-%2520Google%2520Chrome%252029_10_2022%252017_16_10.png%22&x-id=GetObject" className='w-100 md:w-1/2 m-auto pb-16' alt="écran de connexion sportelia" />
+        <Image src={sportelia2} className='w-100 md:w-1/2 m-auto pb-16' alt="écran de connexion sportelia" />
        
         <p className='mb-8'>Nous sommes conscients que chaque entreprise a des exigences particuli&egrave;res, c&apos;est pourquoi notre application est &eacute;volutive et peut &ecirc;tre personnalis&eacute;e selon vos besoins sp&eacute;cifiques. Si vous avez des exigences particuli&egrave;res pour votre marque de sport, nous sommes l&agrave; pour vous aider &agrave; d&eacute;velopper une solution sur mesure.</p>
         <p className='mb-8'>En somme, notre application web de gestion de franchises sportives est l&apos;outil id&eacute;al pour les propri&eacute;taires de marques de sport qui cherchent &agrave; optimiser la gestion de leurs franchises. Avec une interface simple et intuitive, une s&eacute;curit&eacute; renforc&eacute;e, une gestion fine des droits d'acc&egrave;s et des tableaux de bord personnalisables, notre application est la solution id&eacute;ale pour les marques de sport qui cherchent &agrave; se d&eacute;velopper et &agrave; prosp&eacute;rer.</p>  
