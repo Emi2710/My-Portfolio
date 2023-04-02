9b0909c51f96b2a771a079b4674d373e3c8545a8
Emi2710 <deadourfreedom@gmail.com>
2023-04-02 17:49:16 +0200

    implementing google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'YOUR_TRACKING_ID', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
+      </Head>
       <body className=''>
         <Main />
         <NextScript />
